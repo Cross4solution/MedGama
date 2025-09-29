944ab9ffb708403ab3e1044f70b543ffc685b788
mustafadmrsy <125405286+mustafadmrsy@users.noreply.github.com>
2025-09-29 12:22:02 +0300

    .

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
 
   return (
     <>
-    <header className={`fixed top-0 left-0 right-0 z-50 border-b bg-white`}>
+    <header className={`fixed top-0 left-0 right-0 z-50 md:border-b bg-white`}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
         <div className="grid grid-cols-[auto,1fr,auto] items-center gap-4">
           {/* Logo */}
